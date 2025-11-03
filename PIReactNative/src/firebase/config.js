bacdc8942d0540d99272bba93c322f23bd7240e7
Vizanardi <mzanardiocampo@udesa.edu.ar>
2025-11-03 10:18:57 -0300

    Base del proyecto

---
+++ PIReactNative/src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from "firebase/app";
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOAxv3tbRWB2dfPW-wNUaCW4HjyNyYqSo",
+  authDomain: "bdreactnative-83105.firebaseapp.com",
+  projectId: "bdreactnative-83105",
+  storageBucket: "bdreactnative-83105.firebasestorage.app",
+  messagingSenderId: "767681131965",
+  appId: "1:767681131965:web:b45402f23a278e971e0ed4"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
